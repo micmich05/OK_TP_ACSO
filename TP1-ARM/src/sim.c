f51d7185fd507c7865531c7e06a73eff0440915b
ValenPettaz1 <vpettazi@udesa.edu.ar>
2025-03-25 15:38:51 -0300

    falta implementar bien subs y ver por que no levantra

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -9,11 +9,11 @@ bool check_cond(int cond);
 uint64_t zeroExtend(uint64_t imm, int datasize);
 uint64_t addWithCarry(uint64_t x, uint64_t y, int carry_in) ;
 void bcond(uint32_t instr);
+void subis(uint32_t instr);
+
 // Se eliminó la declaración externa de adds_imm, ya que se implementa a continuación
 
 // Funciones integradas de add.c
-
-
 
 void adds_imm(uint32_t instr) {
     int d = (instr >> 0) & 0x1F;         // bits [4:0]: registro destino
@@ -117,11 +117,20 @@ void process_instruction() {
                 else if (strcmp(opcode_dict[j].mnemonic, "B COND") == 0) {
                     bcond(instr);
                 }
+
+                else if (strcmp(opcode_dict[j].mnemonic, "SUB (Immediate)") == 0) {
+                    subis(instr);
+                }
                 // Si la instrucción es HALT se detiene la simulación
                 else if (strcmp(opcode_dict[j].mnemonic, "HALT") == 0) {
                     RUN_BIT = FALSE;
                     exit(0);
                 }
+
+                else if (strcmp(opcode_dict[j].mnemonic, "SUB (Extended register)") == 0) {
+                    subs_register(instr);
+                }
+                
                 // Si es otra instrucción, solo se muestra su mnemónico
                 else {
                     printf("%s\n", opcode_dict[j].mnemonic);
@@ -156,6 +165,89 @@ void bcond (uint32_t instr){
         //Update PC
         NEXT_STATE.PC = CURRENT_STATE.PC + 4;
     }
+}
+
+void subis(uint32_t instr){
+    //Decode
+    int d = (unsigned int)((instr >> 0) & 0x1F);         // bits [4:0]: registro destino
+    int n = (unsigned int)((instr >> 5) & 0x1F);         // bits [9:5]: registro fuente
+    int datasize = 64;
+    int imm12 = (instr >> 10) & 0xFFF;   // bits [21:10]
+    int shift = (instr >> 22) & 0x1;     // solo 1 bit en lugar de 2
+
+    // Inicialmente imm es imm12
+    uint64_t imm = imm12;
+
+    switch (shift) {
+        case 0:
+            imm = zeroExtend(imm, datasize);
+            break;
+        case 1:
+            imm = zeroExtend(imm << 12, datasize);
+            break;
+    }
+
+    // Extiende a datasize (64 bits)
+
+    uint64_t operand1 = CURRENT_STATE.REGS[n];
+    uint64_t operand2 = -(int64_t)imm;
+    uint64_t result = addWithCarry(operand1, imm, 0); //TODO: Ver carry 0
+
+    // Guardar el resultado en el registro destino
+    NEXT_STATE.REGS[d] = result;
+
+    // Los flags FLAG_N y FLAG_Z se actualizan en addWithCarry.
+    // Si es necesario actualizar otros flags, se podría hacer aquí.
+
+    printf("d: %d\n", d);
+    printf("imm: %llu\n", imm);
+    printf("operand1: %llu\n", operand1);
+    printf("result: %llu\n", result);
+
+    //Update PC
+    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+    
+}
+
+void subs_register(uint32_t instr){
+    //Decode
+    int d = (unsigned int)((instr >> 0) & 0x1F);         // bits [4:0]: registro destino
+    int n = (unsigned int)((instr >> 5) & 0x1F);         // bits [9:5]: registro fuente
+    int m = (unsigned int)((instr >> 16) & 0x1F);        // bits [20:16]: registro fuente 2
+    int shift = (instr >> 22) & 0x3;     // 2 bits
+
+    // Inicialmente imm es imm12
+    uint64_t imm = CURRENT_STATE.REGS[m];
+
+    switch (shift) {
+        case 0:
+            imm = zeroExtend(imm, 64);
+            break;
+        case 1:
+            imm = zeroExtend(imm << 12, 64);
+            break;
+    }
+
+    // Extiende a datasize (64 bits)
+
+    uint64_t operand1 = CURRENT_STATE.REGS[n];
+    uint64_t operand2 = -(int64_t)imm;
+    uint64_t result = addWithCarry(operand1, imm, 0); //TODO: Ver carry 0
+
+    // Guardar el resultado en el registro destino
+    NEXT_STATE.REGS[d] = result;
+
+    // Los flags FLAG_N y FLAG_Z se actualizan en addWithCarry.
+    // Si es necesario actualizar otros flags, se podría hacer aquí.
+
+    printf("d: %d\n", d);
+    printf("imm: %llu\n", imm);
+    printf("operand1: %llu\n", operand1);
+    printf("result: %llu\n", result);
+
+    //Update PC
+    NEXT_STATE.PC = CURRENT_STATE.PC + 4;
+    
 }
 
 
@@ -193,7 +285,7 @@ uint64_t zeroExtend(uint64_t imm, int datasize) {
 }
 
 uint64_t addWithCarry(uint64_t x, uint64_t y, int carry_in) {
-    uint64_t unsigned_sum = x + y + carry_in;
+    int unsigned_sum =(unsigned int) x + (unsigned int)y + (unsigned int)carry_in;
     uint64_t result = unsigned_sum & ((1ULL << 64) - 1);
 
     // Actualiza banderas de estado
