63c65444e190cf016fe6764d6a8e71ea20717d62
ValenPettaz1 <vpettazi@udesa.edu.ar>
2025-03-27 14:43:00 -0300

    lsl

--- TP1-ARM/src/sim.c
+++ TP1-ARM/src/sim.c
@@ -422,26 +422,21 @@ void logical_shift (uint32_t instr){
     // Decode
     int d = (instr >> 0) & 0x1F;         // bits [4:0]: registro destino
     int n = (instr >> 5) & 0x1F;         // bits [9:5]: registro fuente
-    int imm6 = (instr >> 10) & 0x3F;     // bits [15:10]
-    int immr = (instr >> 16) & 0x3F;     // bits [21:16]
-    uint64_t result;
+    uint8_t imm6 = (instr >> 10) & 0x3F;     // bits [15:10]
+    uint8_t immr = (instr >> 16) & 0x3F;     // bits [21:16]
 
-    uint64_t operand1 = CURRENT_STATE.REGS[n];
+    
 
     // Si imm6 es 0x3F (todos los 6 bits a 1), se realiza un right shift, 
     // de lo contrario se realiza un left shift.
     if (imm6 == 0x3F) {
-        result = operand1 >> immr;
+        NEXT_STATE.REGS[d] = CURRENT_STATE.REGS[n] >> immr;
     } else {
-        result = operand1 << immr;
+        NEXT_STATE.REGS[d] = CURRENT_STATE.REGS[n] << immr;
     }
 
-    // Guardar el resultado en el registro destino
-    NEXT_STATE.REGS[d] = result;
 
     printf("d: %d\n", d);
-    printf("operand1: %llu\n", operand1);
-    printf("result: %llu\n", result);
 
     // Update PC
     NEXT_STATE.PC = CURRENT_STATE.PC + 4;
