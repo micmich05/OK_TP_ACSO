e775eefd67e2712aa099b0ae9f1ce496d90cf9f4
ValenPettaz1 <vpettazi@udesa.edu.ar>
2025-03-28 15:43:20 -0300

    update br

--- TP1-ARM/inputs/br.s
+++ TP1-ARM/inputs/br.s
@@ -1,7 +1,7 @@
 .text
     // Asumimos que el código se carga en 0x10000000.
     // Queremos que 'target' esté en 0x10000020 (32 bytes después del inicio).
-    movz  X1, 0x10000020    // Carga en X1 la dirección absoluta de 'target'
+    movz  X1, 0x1000    // Carga en X1 la dirección absoluta de 'target'
     br    X1               // Salta a 'target'
     adds  X2, X0, #1       // Estas instrucciones se deben saltar
     adds  X3, X0, #2
