796025ff471555dd13b108623b8ee2c5d7b8a018
ValenPettaz1 <vpettazi@udesa.edu.ar>
2025-03-29 16:00:58 -0300

    update br test

--- TP1-ARM/inputs/br.s
+++ TP1-ARM/inputs/br.s
@@ -1,15 +1,16 @@
 .text
-    // Asumimos que el código se carga en 0x10000000.
-    // Queremos que 'target' esté en 0x10000020 (32 bytes después del inicio).
-    movz  X1, 0x1000    // Carga en X1 la dirección absoluta de 'target'
-    br    X1               // Salta a 'target'
-    adds  X2, X0, #1       // Estas instrucciones se deben saltar
-    adds  X3, X0, #2
-    adds  X4, X0, #3
-    adds  X5, X0, #4
-    adds  X6, X0, #5
-    adds  X7, X0, #6
+    ADD X0, X0, 0X4
+    LSL X0, X0, 20
+    ADD X0, X0, 20
+    BR X0
+    B label1
+    HLT 0
 
-target:
-    HLT   0
-
+label1:
+    ADD X1, X2, 10                 
+    BR X0                           
+    ADD X2,X3,1
+    ADD X3,X4,1
+    ADD X4,X5,1
+    ADD X5,X6,1
+    HLT 0
